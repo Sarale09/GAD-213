b6a1608078f75eea4375a01f9493cf74bd6cb309
Sarale09 <alejandrasarmiento0998@gmail.com>
2025-02-28 15:15:12 +1100

    Parallax fully working. Needs to be connected to player's movement

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -7,8 +7,10 @@ public class Parallax : MonoBehaviour
     [SerializeField] private Material[] BgMaterials;
     public float x;
     private float[] parallaxScales;
+    public float xScale;
     void Start()
     {
+        //creates different scales for the movement of the different layers
         parallaxScales = new float[BgMaterials.Length];
 
         for (int i = 0; i < parallaxScales.Length; i++)
@@ -19,9 +21,14 @@ public class Parallax : MonoBehaviour
 
     void Update()
     {
+        //for testing purposes
+        //x will have to be changed to be conencted to the player's x value
+        x += Time.deltaTime;
+
+        //causes the layers to be offset by the amount determined by their scales
         for (int i = 0;i < parallaxScales.Length;i++)
         {
-            float parallax = parallaxScales[i] * x;
+            float parallax = parallaxScales[i] * x * xScale;
 
             Vector2 backgroundTargetPos = new Vector2(parallax, BgMaterials[i].mainTextureOffset.y);
 
