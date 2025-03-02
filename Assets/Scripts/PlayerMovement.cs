a0e555a35793215bb169993d8ce57dd8a98d0253
Sarale09 <alejandrasarmiento0998@gmail.com>
2025-03-02 16:04:11 +1100

    back and forth movement with scale changes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -12,24 +13,18 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D)) 
+
+        //walk right if pressing D
+        if (Input.GetKeyDown(KeyCode.D))
         {
             animator.SetBool("WalkRight", true);
-            //animator.parameters("WalkRight") = true;
-            //animator.Play("Anim_RightTurn");
-            //if (Input.GetKey(KeyCode.D))
-            //{
-            //    animator.Play("Anim_RightWalk");
-            //}
-            //else
-            //{
-            //    animator.Play("Idle");
-            //}
-        } else if (Input.GetKeyUp(KeyCode.D))
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
         {
             animator.SetBool("WalkRight", false);
         }
 
+        //walk left if pressing A
         if (Input.GetKeyDown(KeyCode.A))
         {
             animator.SetBool("WalkLeft", true);
@@ -39,6 +34,21 @@ public class PlayerMovement : MonoBehaviour
             animator.SetBool("WalkLeft", false);
         }
 
+        //walk further away
+        if (Input.GetKey(KeyCode.W) && transform.position.y < -2)
+        {
+            //Debug.Log("Up");
+            transform.Translate(new Vector3(0,1,0) * Time.deltaTime);
+            transform.localScale -= new Vector3(0.3f, 0.3f, 0.3f) * Time.deltaTime;
+        }
+
+        //walk closer
+        if (Input.GetKey(KeyCode.S) && transform.position.y > -3.25)
+        {
+            //Debug.Log("Down");
+            transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime);
+            transform.localScale += new Vector3(0.3f,0.3f,0.3f) * Time.deltaTime;
+        }
 
     }
 }
