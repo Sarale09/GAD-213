c9d856bef9113299bddf4d66d84fc2231b0869b3
Sarale09 <alejandrasarmiento0998@gmail.com>
2025-04-01 15:18:11 +1100

    brew and overworld switch UI implemented

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,16 +5,34 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public bool overworldView;
-    public bool potionView;
+    public bool brewView;
+
+    public GameObject overworld;
+    public GameObject brew;
     // Start is called before the first frame update
     void Start()
     {
-        
+        overworldView = true;
+        brewView = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void BrewViewButton()
+    {
+        overworldView = false;
+        overworld.SetActive(false);
+        brewView = true;
+        brew.SetActive(true);
+    }
+    public void OverworldViewButton()
+    {
+        overworldView = true;
+        overworld.SetActive(true);
+        brewView = false;
+        brew.SetActive(false);
     }
 }
